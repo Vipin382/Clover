7fab61290d7a60402e84b1f4b9e2e9e277b9aac7
Vipin Bhati <vipin.bhati2020@vitstudent.ac.in>
2022-05-15 18:53:18 +0530

    Add files via upload

---
+++ javascript/shoes.js
@@ -0,0 +1,10 @@
+let shoes = [
+    {src:"shoes/1.png"},
+    {src:"shoes/2.png"},
+    {src:"shoes/3.png"},
+    {src:"shoes/4.png"},
+    {src:"shoes/5.png"},
+    {src:"shoes/6.png"},
+    {src:"shoes/7.png"},
+    {src:"shoes/8.png"}
+]
